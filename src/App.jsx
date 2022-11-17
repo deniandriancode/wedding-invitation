67fec5c7b3e8e9c601455c9d8a4cfc725ef13fa8
Deni Andrian Prayoga <deniandriancode@gmail.com>
2022-11-17 21:24:06 +0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import { useState } from 'react';
+import { encrypt, decrypt } from "../utils/cryptography.js";
 
 function App() {
-	return <div></div>;
+	return <div className="text-emerald-800">{encrypt("ytu7263bcw907qe05b87023b7028b73b50v8730475vb08734v05", "John Doe")}</div>;
 }
 
 export default App;
---
+++ utils/cryptography.js
@@ -0,0 +1,47 @@
+"use strict";
+
+function getPRNG(seed) {
+	let seedValue = []
+	for (let i in seed) {
+		seedValue.push(seed.charCodeAt(i));
+	}
+	seedValue = seedValue.reduce((acc, x) => acc + x, 0);
+	let randomValue = ((seedValue * 273645) + 823476) % 736467 % 16;
+	return parseInt(randomValue);
+}
+
+function encrypt(salt, message) {
+	let index = parseInt(message.length / 2);
+	let msg = message.slice(0, index) + salt + message.slice(index);
+	let shift = getPRNG(salt);
+	let numMessage = [];
+
+	for (let i in msg) {
+		numMessage.push(msg.charCodeAt(i) + shift);
+	}
+
+	let encryptedMessage = numMessage.map(c => String.fromCharCode(c)).join("");
+	return encryptedMessage;
+}
+
+function decrypt(salt, message) {
+	let encSalt = []
+	let shift = getPRNG(salt);
+
+	for (let i in salt) {
+		encSalt.push(salt.charCodeAt(i) + shift);
+	}
+
+	encSalt = encSalt.map(c => String.fromCharCode(c)).join("");
+	let encMessage = message.replace(encSalt, "");
+	let numMessage = [];
+
+	for (let i in encMessage) {
+		numMessage.push(encMessage.charCodeAt(i) - shift);
+	}
+
+	let decryptedMessage = numMessage.map(c => String.fromCharCode(c)).join("");
+	return decryptedMessage;
+}
+
+export { encrypt, decrypt };
