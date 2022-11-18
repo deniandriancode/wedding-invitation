0c5e1a6db1db60257501abf47149c2bccc669633
Deni Andrian Prayoga <deniandriancode@gmail.com>
2022-11-18 19:57:44 +0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -91,9 +91,25 @@ function HomePage(props) {
 
 function Couple(props) {
 	return (
-		<div className="min-w-fit flex items-center -mt-10 relative z-index-0 min-h-screen px-2 bg-cover bg-center bg-no-repeat bg-fixed">
+		<div className="min-w-fit flex flex-col justify-center items-center -mt-10 relative z-index-0 min-h-screen px-2 bg-cover bg-center bg-no-repeat bg-fixed">
 		{/* <div className="min-w-fit relative z-index-0 min-h-screen bg-[url('/src/assets/bg-vec.jpg')] bg-cover bg-center bg-no-repeat bg-fixed"> */ }
-			<div className="container border max-w-xl md:mx-auto h-screen my-24 text-center relative z-index-1">
+			<div className="container border max-w-xl md:mx-auto bg-yellow-100 h-screen my-24 text-center relative z-index-1">
+				<h1 dir="rtl">بِسْمِ اللَّهِ الرَّحْمَنِ الرَّحِيْمِ</h1>
+				<h2>Assalamualaikum Warahmatullahi Wabarakatuh</h2>
+				<p>Maha suci Allah Subhanahu wa ta'ala yang telah menciptakan makhluk-Nya berpasang-pasangan.</p>
+				<p dir="rtl">
+وَمِنْ آيَاتِهِ أَنْ خَلَقَ لَكُمْ مِنْ أَنْفُسِكُمْ أَزْوَاجًا لِتَسْكُنُوا إِلَيْهَا وَجَعَلَ بَيْنَكُمْ مَوَدَّةً وَرَحْمَةً ۚ إِنَّ فِي ذَٰلِكَ لَآيَاتٍ لِقَوْمٍ يَتَفَكَّرُونَ ﴿ ٢١﴾
+				</p>
+				<p>[30:21] Dan di antara tanda-tanda kekuasaan-Nya ialah Dia menciptakan untukmu isteri-isteri dari jenismu sendiri, supaya kamu cenderung dan merasa tenteram kepadanya, dan dijadikan-Nya diantaramu rasa kasih dan sayang. Sesungguhnya pada yang demikian itu benar-benar terdapat tanda-tanda bagi kaum yang berfikir.</p>
+			</div>
+
+			<div className="container flex flex-wrap gap-5 md:flex-nowrap border max-w-4xl md:mx-auto bg-yellow-100 h-screen mb-24 text-center relative z-index-1">
+				<div className="flex bg-blue-100">
+					<h1>Aliquet nibh praesent tristique magna sit amet purus gravida quis blandit turpis cursus in hac habitasse platea dictumst quisque sagittis!</h1>
+				</div>
+				<div className="flex bg-blue-100">
+					<h1>Aliquet nibh praesent tristique magna sit amet purus gravida quis blandit turpis cursus in hac habitasse platea dictumst quisque sagittis!</h1>
+				</div>
 			</div>
 		</div>
 	);
