6eeb729be7f1c5bebdda77d091ef34cb55b002d5
Deni Andrian Prayoga <deniandriancode@gmail.com>
2023-02-10 15:46:47 +0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -55,7 +55,7 @@ const initTabs = [
 ];
 
 function App() {
-  const salt = "cdef";
+  const salt = "aaaa";
   const encGuestName = getQueryString("guest");
   const decGuestName = decrypt(salt, encGuestName).replaceAll("_", " ");
 
