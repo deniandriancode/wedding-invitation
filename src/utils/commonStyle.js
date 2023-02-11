a959b61c4f4894025dd219aee506712822a3cf9b
Deni Andrian Prayoga <deniandriancode@gmail.com>
2023-02-11 19:10:11 +0700

    update

--- src/utils/commonStyle.js
+++ src/utils/commonStyle.js
@@ -1,4 +1,4 @@
 import 'animate.css';
 
 export const rootClass = 'flex justify-center items-center min-h-screen border-box bg-purple-200 bg-center bg-cover';
-export const containerClass = 'animate__animated animate__slideInUp animate__slow rounded-md max-w-[1024px] w-[90%] sm:w-[60%] text-center text-gray-700 py-24 px-4 shadow-custom bg-purple-300 border-gray-50 border border-purple-600';
+export const containerClass = 'animate__animated animate__fadeInUp animate__slow rounded-md max-w-[1024px] w-[90%] sm:w-[60%] text-center text-gray-700 py-24 px-4 shadow-custom bg-purple-300 border-gray-50 border border-purple-600';
